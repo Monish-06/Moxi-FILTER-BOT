7abbd6084df1f06134c2f26271b9a3487ef72dbe
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-03 11:08:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -161,7 +161,7 @@ STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "60"))  # 1 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
 else:
