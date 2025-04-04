f8578dc19b65f9b532a29efeecfc089bda47cf1d
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-04 09:55:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import threading
              requests.get("https://sleepy-starfish-monish2807-50b1cc37.koyeb.app/")
          except:
              pass
-         time.sleep(30)  # Ping every 5 minutes
+         time.sleep(120)  # Ping every 5 minutes
  
  threading.Thread(target=keep_alive, daemon=True).start()
 # Clone Code Credit : YT - @Tech_VJ / TG - @VJ_Bots / GitHub - @VJBots
