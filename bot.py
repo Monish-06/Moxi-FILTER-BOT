785ed5ab39487cb66b3af2543b105ad89f16a2f1
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-04 13:25:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ def keep_alive():
             requests.get("https://sleepy-starfish-monish2807-50b1cc37.koyeb.app/")
         except:
             pass
-        time.sleep(100)  # Ping every 5 minutes
+        time.sleep(200)  # Ping every 5 minutes
  
 threading.Thread(target=keep_alive, daemon=True).start()
 
