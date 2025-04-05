3d2e0cac9aab4d7b4d00e0fb6c8c0cbc6ec1b0e0
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-05 23:18:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from filestore.filestore_handler import handle_file  # this auto registers the handler
+
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
