1cf4cd72b330b7edec4f1f6d6f8bbb526c354989
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-05 21:16:18 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -28,8 +28,8 @@ async def gen_link_s(bot, message):
     if message.has_protected_content and message.chat.id not in ADMINS:
         return await message.reply("okDa")
     result = unpack_new_file_id(getattr(replied, file_type.value).file_id)
-     file_id = result[0]
-     ref = result[1] if len(result) > 1 else None
+    file_id = result[0]
+    ref = result[1] if len(result) > 1 else None
      #file_id, ref = unpack_new_file_id(getattr(replied, file_type.value).file_id)
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
