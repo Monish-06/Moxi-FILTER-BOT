2d12c8489010b62bea418f37fa50a4d2f402e37d
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-27 22:47:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2600,7 +2600,8 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
         req = message.from_user.id if message.from_user else 0
         FRESH[key] = search
         temp.GETALL[key] = files
-        temp.SHORT[message.from_user.id] = message.chat.id
+        user_id = message.from_user.id if message.from_user else message.chat.id
+        temp.SHORT[user_id] = message.chat.id
         if settings["button"]:
             btn = [
                 [
